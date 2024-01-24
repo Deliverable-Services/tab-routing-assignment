1560d76afac1688c4846bf34ffb1ecc10f6e6483
DigiDealsDev <aashish@deliverable.services>
2024-01-24 08:48:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-center text-xl font-semibold mt-8">
+        This is demo to show the routing using the tabs.
+      </p>
+    </div>
+  );
+}
